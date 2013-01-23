e5612b05eadfe20fa501d527bb21c33bd4bc4576
Cory Crabb <ccrabb@gmail.com>
2013-01-22 23:20:35 -0800

    Minor layout fixes

--- Beer Boutique/Controllers/HomeController.cs
+++ Beer Boutique/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace BeerBoutique.Controllers
 
         public ActionResult Index() {
             ViewBag.InfoMessage = "Beer Boutique is undergoing maintenance from now until forever.  Thanks for your understanding.";
-
+            ViewBag.Title = "GOMBUI - Cheers";
             //WebSecurity.Login("test", "test", true);
             //WebSecurity.CreateUserAndAccount("test", "test");
             return View();
